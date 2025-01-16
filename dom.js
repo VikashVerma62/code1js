d7925538d9ac22cf6910cf2f783645181224c1ec
VikashVerma62 <vikashverma626506@gmail.com>
2025-01-16 01:16:45 -0800

    update

---
+++ dom.js
@@ -0,0 +1,18 @@
+ document.getElementById('title'); //select an html element by id
+ document.getElementById('title').id // to find id
+ document.getElementById('title').className  // to find class name or class
+ document.getElementById('title').getAttribute   // to find the attribute
+ 
+ document.getElementById('title').setAttribute('class','test')  // it alway overight
+ const title=document.getElementById('title');
+ title.style.backgroundColor="green";
+ title.style.padding="15px";
+
+
+
+//  tittle.textContent   // it gives all hidden or visible values ex ========> display:"none"; is also visible in innerContent
+// tittle.innerHTML     // it works only on available values
+// title.innerText      
+
+
+// difference between innerText and textContent
---
+++ truthy.js
@@ -0,0 +1,24 @@
+const userEmail=[];
+if(userEmail){
+    console.log("Got user email");
+}
+else{
+    console.log("don't have email");
+}
+
+// falsy values
+// false ,0 ,-0 , BigIntc 0n , null, undefined, NaN
+// truthy values 
+//  "0"  , 'false' , " " , [] , {}, function(){}, 
+// 
+
+
+
+
+
+// Nullish Coalescing Operater(??) :Null undefind
+let val1;
+val1=5 ?? 10;
+val1 =null ?? 10;
+console.log(val1);
+
