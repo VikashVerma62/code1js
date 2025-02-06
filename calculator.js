ff19bcb473431c82b15887399854cc1a07a41298
VikashVerma62 <vikashverma626506@gmail.com>
2025-02-06 04:46:08 -0800

    update

--- calculator.js
+++ calculator.js
@@ -2,6 +2,24 @@ let store="";
 let cal=(value)=>{
     store=store+value 
     let show=document.querySelector("#display")
-    show.innerHTML=show
+    show.innerHTML=store;
+
+}
+let res=()=>{
+    let show=document.querySelector("#display");
+    show.innerHTML=eval(store)
+    store.toString()
+}
+
+let AC=()=>{
+    store="";
+    let show=document.querySelector("#display");
+    show.innerHTML=store
+}
+
+let del=()=>{
+    let show=document.querySelector("#display")
+    store=store.slice(0,-1);
+    show.innerHTML=store; 
 
 }
