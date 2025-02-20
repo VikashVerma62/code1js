0daee1c3eaa64e1dfa67812bc5f3757a7b49bb96
VikashVerma62 <vikashverma626506@gmail.com>
2025-02-20 05:37:10 -0800

    update

--- comparision.js
+++ comparision.js
@@ -3,12 +3,12 @@ console.log(2>=1);
 console.log(2==2);
 console.log(2!=2);
 
-
-
-
 console.log("2">1);
 console.log("02">2);
 
 // comparisons covert null to a number treating as 0 .
 // that's why (2)>=0 gives true null (1)>0 false
 console.log("2"===2);
+
+console.log("3"===3);
+console.log(4==="4");
