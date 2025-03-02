822ca8d4b11b2762bf329db91dd61f40bacc43de
VikashVerma62 <vikashverma626506@gmail.com>
2025-03-01 20:08:10 -0800

    update

--- localstorage.js
+++ localstorage.js
@@ -2,6 +2,7 @@ let run1=()=>{
     localStorage.setItem("Name", "vikash");
     localStorage.setItem("age", "20")
     localStorage.setItem("cgpa", "6.5");
+    localStorage.setItem("sgpa","7.5")
 
 }
 let show=document.querySelector("#show")
@@ -11,4 +12,5 @@ show.innerHTML=localStorage.getItem("Name")
 
 let logout=()=>{
     localStorage.removeItem("Name");
+    localStorage.removeItem("age");
 }
